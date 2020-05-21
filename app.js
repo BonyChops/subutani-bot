2b80dd0b1f61a744427645884b2b8f248cba8832
Bony_Chops <office.kentarou@gmail.com>
2020-05-21 15:19:13 +0900

    Edit

--- app.js
+++ app.js
@@ -20,9 +20,6 @@ const embedAlert = (name, description, color, time, userIcon, fields = []) =>{
 function execShellCommand(cmd) {
   return new Promise((resolve, reject) => {
    exec(cmd, (error, stdout, stderr) => {
-    if (error) {
-     console.warn(error);
-    }
     resolve(stdout? stdout : stderr);
    });
   });
